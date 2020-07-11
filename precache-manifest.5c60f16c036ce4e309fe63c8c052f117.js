0690d7f37be8de1d154cdc302c601c13929d2eb0
mhawad <mirnahelmybio@yahoo.com>
2020-07-10 23:43:29 -0400

    Add files via upload

---
+++ precache-manifest.5c60f16c036ce4e309fe63c8c052f117.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3daad6e99e962f87ad78a3ef6a320529",
+    "url": "/mhsokar/WEB215/Lesson14/index.html"
+  },
+  {
+    "revision": "1444c65644a1f6c1ad98",
+    "url": "/mhsokar/WEB215/Lesson14/static/css/main.beaf461c.chunk.css"
+  },
+  {
+    "revision": "d73ea675c45b43a980ec",
+    "url": "/mhsokar/WEB215/Lesson14/static/js/2.5c20e2d1.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/mhsokar/WEB215/Lesson14/static/js/2.5c20e2d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1444c65644a1f6c1ad98",
+    "url": "/mhsokar/WEB215/Lesson14/static/js/main.b0854e84.chunk.js"
+  },
+  {
+    "revision": "9934c019b8a7d8cd64cf",
+    "url": "/mhsokar/WEB215/Lesson14/static/js/runtime-main.5845026d.js"
+  },
+  {
+    "revision": "4ab30596f2bf78aadf7e0c653252e551",
+    "url": "/mhsokar/WEB215/Lesson14/static/media/mirna.4ab30596.jpg"
+  }
+]);
